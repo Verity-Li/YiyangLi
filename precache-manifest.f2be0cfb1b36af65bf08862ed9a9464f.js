19fbfd4dbc75575297ee3e508fd95351c41b5d17
Verity-Li <veritylaufeyson@163.com>
2022-03-03 00:24:14 +0800

    Updates

---
+++ precache-manifest.f2be0cfb1b36af65bf08862ed9a9464f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9492ad581891a8086ce6",
+    "url": "/YiyangLi/static/css/main.207c3bcc.chunk.css"
+  },
+  {
+    "revision": "9492ad581891a8086ce6",
+    "url": "/YiyangLi/static/js/main.9492ad58.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/YiyangLi/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "f5c7949a2c78906b2be4",
+    "url": "/YiyangLi/static/js/2.f5c7949a.chunk.js"
+  },
+  {
+    "revision": "5561c96c3a14b043550d",
+    "url": "/YiyangLi/static/js/runtime~main.5561c96c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/YiyangLi/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d869f3aca4a116e223a5642a98e20893",
+    "url": "/YiyangLi/static/media/Arrow.d869f3ac.svg"
+  },
+  {
+    "revision": "81b3493daf687d4520c3f6e99a225391",
+    "url": "/YiyangLi/static/media/Face.81b3493d.png"
+  },
+  {
+    "revision": "2d03b86554ac6db10b0e6a15ac8a90bc",
+    "url": "/YiyangLi/static/media/ExperienceDesign.2d03b865.png"
+  },
+  {
+    "revision": "b2df11cd31d494234543487bf10ffaa9",
+    "url": "/YiyangLi/static/media/Cili.b2df11cd.png"
+  },
+  {
+    "revision": "231b0c1904baea755c8b4c35f5fe35ad",
+    "url": "/YiyangLi/static/media/TakeMeOut.231b0c19.png"
+  },
+  {
+    "revision": "496d7bfad7c4ee4c45fdf2c318df2bf7",
+    "url": "/YiyangLi/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/YiyangLi/precache-manifest.f2be0cfb1b36af65bf08862ed9a9464f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/YiyangLi/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
